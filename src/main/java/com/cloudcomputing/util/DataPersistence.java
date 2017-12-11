20d431ab0d1329fdf18be20e5b37010499154224
Wangsiwei01 <sw3453@nyu.edu>
2017-12-11 15:10:54 -0500

    add user signup jdbc

--- src/main/java/com/cloudcomputing/util/DataPersistence.java
+++ src/main/java/com/cloudcomputing/util/DataPersistence.java
@@ -11,11 +11,12 @@ public class DataPersistence {
     private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
     private static final String DB_URL = "jdbc:mysql://ccproject.cumbn0blorkz.us-east-1.rds.amazonaws.com";
     
-    public static boolean loginCheck(String username, String password) {
-        if (username == null || password == null) {
+    public static boolean LoginCheck(String username, String pass) {
+        if (username == null || pass == null) {
             throw new IllegalArgumentException("Username and Password can not be null!");
         }
-        Connection conn = null;
+        Connection conn = null;       
+        String password = PasswordHashing.getPasswordAfterHashing(pass);
         
         try {
             Class.forName(JDBC_DRIVER);
@@ -24,7 +25,7 @@ public class DataPersistence {
             conn = DriverManager.getConnection(DB_URL, "ccproject", "12345678");
             
             System.out.println("Creating statement...");  
-            PreparedStatement selectPassword = conn.prepareStatement("Select password from finalproject.user where name = ?");
+            PreparedStatement selectPassword = conn.prepareStatement("Select password from finalproject.user where username = ?");
             selectPassword.setString(1, username);
             ResultSet rs = selectPassword.executeQuery(); 
             while (rs.next()) {
@@ -45,5 +46,54 @@ public class DataPersistence {
             }
         }
         return false;
-    }   
+    }
+    
+    
+    public static boolean UserSignUp(String username, String pass, String email, String phone) {
+        if (username == null || pass == null) {
+            throw new IllegalArgumentException("Username and Password can not be null!");
+        }
+        Connection conn = null;
+        String password = PasswordHashing.getPasswordAfterHashing(pass);
+        
+        try {
+            Class.forName(JDBC_DRIVER);
+
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, "ccproject", "12345678");
+            
+            //TODO check existing and insert
+            
+            System.out.println("Creating statement to check if exsiting...");  
+            PreparedStatement selectUser = conn.prepareStatement("Select * from finalproject.user where username = ?");
+            selectUser.setString(1, username);
+            ResultSet rs = selectUser.executeQuery();
+            if (rs.next()) {
+                System.out.println("User already exsits!");  
+                return false;
+            }
+            System.out.println("Creating statement to insert...");  
+            
+            PreparedStatement insertNewUser = conn.prepareStatement("Insert into finalproject.user (username, password, email, phone) values (?, ?, ?, ?)");
+            insertNewUser.setString(1, username);
+            insertNewUser.setString(2, password);
+            insertNewUser.setString(3, email);
+            insertNewUser.setString(4, phone);
+            int rs2 = insertNewUser.executeUpdate();
+            System.out.println("Inserted! " + rs2 + "rows added!");  
+            return true;
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    } 
 }
